7d10751c3ed85940b753ef6d7db8ca94052c4a93
JiahaoZhan <Ichirojp201@gmail.com>
2021-02-20 06:18:00 -0500

    Add DateRangePicker

--- src/app.js
+++ src/app.js
@@ -6,14 +6,8 @@ import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import configureStore from "./store/configureStore";
 import { addExpense } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
-import getVisibleExpenses from "./selectors/expenses";
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: "WaterBill", amount: 4500 }));
-store.dispatch(addExpense({ description: "GasBill", createdAt: 1000 }));
-store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
 
 const jsx = (
   <Provider store={store}>
--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,10 +1,18 @@
+import moment from "moment";
 const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
   return expenses
     .filter((expense) => {
-      const startDateMatch =
-        typeof startDate !== "number" || expense.createdAt >= startDate;
-      const endDateMatch =
-        typeof endDate !== "number" || expense.createdAt <= endDate;
+      console.log(expense.createdAt);
+      const createdAtMoment = moment(expense.createAt);
+      console.log(createdAtMoment);
+      const startDateMatch = startDate
+        ? startDate.isSameOrBefore(createdAtMoment, "day")
+        : true;
+
+      const endDateMatch = endDate
+        ? endDate.isSameOrAfter(createdAtMoment, "day")
+        : true;
+
       const textMatch = expense.description
         .toLowerCase()
         .includes(text.toLowerCase());
